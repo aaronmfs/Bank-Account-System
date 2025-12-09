d2e8305820722c8f15980179cba4745b81f54519
Aaron Mathew F. Sinay <sinay.aaronmathew@gmail.com>
2025-12-09 16:36:55 +0800

    refactor: Replace wxWidgets with Win32 API

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,53 @@
-#include <wx/wx.h>
+#include <windows.h>
 
-int main() {
-  std::cout << "Hello, World!" << std::endl;
+// Window procedure - handles events
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
+                            LPARAM lParam) {
+  switch (uMsg) {
+  case WM_DESTROY:
+    PostQuitMessage(0);
+    return 0;
+  case WM_PAINT: {
+    PAINTSTRUCT ps;
+    HDC hdc = BeginPaint(hwnd, &ps);
+    FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
+    EndPaint(hwnd, &ps);
+    return 0;
+  }
+  }
+  return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nCmdShow) {
+  (void)hPrevInstance;
+  (void)lpCmdLine;
+  // Register window class
+  const char CLASS_NAME[] = "MyWindowClass";
+
+  WNDCLASS wc = {};
+  wc.lpfnWndProc = WindowProc;
+  wc.hInstance = hInstance;
+  wc.lpszClassName = CLASS_NAME;
+
+  RegisterClass(&wc);
+
+  // Create window
+  HWND hwnd = CreateWindowEx(0, CLASS_NAME, "Bank Account System",
+                             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+                             800, 600, NULL, NULL, hInstance, NULL);
+
+  if (hwnd == NULL)
+    return 0;
+
+  ShowWindow(hwnd, nCmdShow);
+
+  // Message loop
+  MSG msg = {};
+  while (GetMessage(&msg, NULL, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
   return 0;
 }
