9b8c912bbfd4b533faac49ccc2ed8fbd3b5b5756
Aaron Mathew F. Sinay <sinay.aaronmathew@gmail.com>
2025-12-13 13:18:29 +0800

    test: trying github actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,52 +1,22 @@
-#include <windows.h>
+#include "webview/webview.h"
 
-// Window procedure - handles events
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam,
-                            LPARAM lParam) {
-  switch (uMsg) {
-  case WM_DESTROY:
-    PostQuitMessage(0);
-    return 0;
-  case WM_PAINT: {
-    PAINTSTRUCT ps;
-    HDC hdc = BeginPaint(hwnd, &ps);
-    FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-    EndPaint(hwnd, &ps);
-    return 0;
-  }
-  }
-  return DefWindowProc(hwnd, uMsg, wParam, lParam);
-}
+#include <iostream>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-                   LPSTR lpCmdLine, int nCmdShow) {
-  (void)hPrevInstance;
-  (void)lpCmdLine;
-  // Register window class
-  const char CLASS_NAME[] = "MyWindowClass";
-
-  WNDCLASS wc = {};
-  wc.lpfnWndProc = WindowProc;
-  wc.hInstance = hInstance;
-  wc.lpszClassName = CLASS_NAME;
-
-  RegisterClass(&wc);
-
-  // Create window
-  HWND hwnd = CreateWindowEx(0, CLASS_NAME, "Bank Account System",
-                             WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
-                             800, 600, NULL, NULL, hInstance, NULL);
-
-  if (hwnd == NULL)
-    return 0;
-
-  ShowWindow(hwnd, nCmdShow);
-
-  // Message loop
-  MSG msg = {};
-  while (GetMessage(&msg, NULL, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+#ifdef _WIN32
+int WINAPI WinMain(HINSTANCE /*hInst*/, HINSTANCE /*hPrevInst*/,
+                   LPSTR /*lpCmdLine*/, int /*nCmdShow*/) {
+#else
+int main() {
+#endif
+  try {
+    webview::webview w(false, nullptr);
+    w.set_title("Basic Example");
+    w.set_size(480, 320, WEBVIEW_HINT_NONE);
+    w.set_html("Thanks for using webview!");
+    w.run();
+  } catch (const webview::exception &e) {
+    std::cerr << e.what() << '\n';
+    return 1;
   }
 
   return 0;
